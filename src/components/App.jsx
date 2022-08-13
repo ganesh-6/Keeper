56721567c075e3f2506d670b7de7424cc8d926b7
ganesh <ganesh.22010331@viit.ac.in>
2022-08-13 23:06:16 +0530

    Started with the designing of the keeper app

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+import notes from "../notes";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {notes.map( note => <Note key = {note.key} title = {note.title} content = {note.content} />)}
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
